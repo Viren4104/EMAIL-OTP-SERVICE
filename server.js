94dd1db9307f0dfff78f46c54e87681f9236fae1
Viren4104 <virenpatel4104@gmail.com>
2026-01-09 14:54:43 +0530

    add root route

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ connectDB();
 
 // 2. Setup Routes
 app.use('/api/auth', authRoutes);
-app.get('/', (req, res) => {
-    res.send('Backend is running successfully!');
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
